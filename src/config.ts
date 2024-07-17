4c368bc8b20222a8231d193d3a3c99a2d0e0d1f5
zackproject <zackprojectapp@gmail.com>
2024-07-18 00:05:55 +0200

    Value localstorage

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 // config.ts
+export const API_KEY = "API_KEY";
 export const loc = {
     BASE_URL: "https://api.groq.com/openai/v1",
     MODEL: "llama3-8b-8192",
-    API_KEY: process.env.API_KEY
+    API_KEY: window.localStorage.getItem(API_KEY)
 }
