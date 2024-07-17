8bd9dd21816ab54f9f520ed7371fccf17c111a9b
zackproject <zackprojectapp@gmail.com>
2024-07-17 23:24:38 +0200

    Test process env

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
 // config.ts
-import { env } from "../env.ts" // import.meta.env & process.env not work here
 export const loc = {
     BASE_URL: "https://api.groq.com/openai/v1",
     MODEL: "llama3-8b-8192",
-    API_KEY: env.API_KEY
+    API_KEY: process.env.API_KEY
 }
